ae251b6f6ef787692627730d683522a761a86e21
Suneth <sunethk221@gmail.com>
2024-06-27 15:40:29 +0530

    Changed added

--- Demo.java
+++ Demo.java
@@ -5,7 +5,8 @@ public class Demo {
 
         System.out.println("Array elements:");
         for (int number : numbers) {
-            System.out.println(number);
+            System.out.println("Element" + number);
+    
         }
     }
 }
