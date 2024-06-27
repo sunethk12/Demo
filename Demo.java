24f57b77b801b98a7cdb8bb661d35308560ff8ec
Suneth <sunethk221@gmail.com>
2024-06-27 11:16:43 +0530

    Demo File Added

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+    
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
