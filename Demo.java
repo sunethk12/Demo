923633343d5fa848c051c1fe3a3e6c6d55fc14f0
Suneth <sunethk221@gmail.com>
2024-06-27 12:15:56 +0530

    New change added

--- Demo.java
+++ Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 
         System.out.println("Array elements:");
         for (int number : numbers) {
-            System.out.println(number);
+            System.out.println(number+ "added");
         }
     }
 }
